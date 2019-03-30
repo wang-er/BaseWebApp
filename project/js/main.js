25d57826efb026c3b18360bbf311c5b2a6071469
wang-er <33679181+wang-er@users.noreply.github.com>
2019-03-30 13:52:46 -0400

    seeing if pushing loggin in works

--- project/js/main.js
+++ project/js/main.js
@@ -9,3 +9,19 @@ function myFunction() {
   $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
 }
 
+// Gets called whenever the user clicks "sign in" or "sign out".
+function toggleSignIn() {
+  if (!firebase.auth().currentUser) { // if the user's not logged in, handle login
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/plus.login');
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      console.log("success");
+    }).catch(function(error) {
+      console.error("error", error);
+    });
+  } else { // handle logout
+    firebase.auth().signOut();
+  }
+  //This disables the button until login or logout is successful. You'll want to replace 'login-button' with the id of your login button.
+  $('#login-button').attr("disabled", true);
+}
